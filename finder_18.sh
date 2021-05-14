00ceda72e68caf4273e16de25afeda0dc610ee40
Vijayalaxmi Cigala <vijaya.cigala@gmail.com>
2021-05-14 16:19:35 +0530

    Add district param

--- finder_18.sh
+++ finder_18.sh
@@ -7,9 +7,9 @@ results=''
 
 while [ $ctr -lt 3 ]
 do
-  temp=`/usr/bin/curl -s --location --request GET "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=363&date=${dt}" --header 'Host: cdn-api.co-vin.in' --header 'User-Agent: Mozilla' --header 'Cookie: troute=t1;'  | /usr/local/bin/jq '.centers' | /usr/local/bin/jq '.[] | {pincode: .pincode, date: .sessions[].date, min_age_limit: .sessions[].min_age_limit, available_capacity: .sessions[].available_capacity} | select(.min_age_limit < 45) | select(.available_capacity > 0)' | xargs`
+  temp=`/usr/bin/curl -s --location --request GET "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=$1&date=$dt" --header 'Host: cdn-api.co-vin.in' --header 'User-Agent: Mozilla' --header 'Cookie: troute=t1;'  | /usr/local/bin/jq '.centers' | /usr/local/bin/jq '.[] | {pincode: .pincode, date: .sessions[].date, min_age_limit: .sessions[].min_age_limit, available_capacity: .sessions[].available_capacity} | select(.min_age_limit < 45) | select(.available_capacity > 0)' | xargs`
 
-  results="${results}${temp}"
+  results="$results$temp"
   inc=`expr $ctr \* 7`
   dt=`date -v +"$inc"d +"%d-%m-%Y"`
   ctr=`expr $ctr + 1`
