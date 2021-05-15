7fc27766484ce3f92d79e73b72468ecab8aca3ec
Vijayalaxmi Cigala <vijaya.cigala@gmail.com>
2021-05-15 11:54:43 +0530

    Update finder_45.sh

--- finder_45.sh
+++ finder_45.sh
@@ -2,20 +2,20 @@
 dt=`date +"%d-%m-%Y"`
 
 ctr=0
-results=''
+results='[]'
 
 while [ $ctr -lt 3 ]
 do
-  temp=`/usr/bin/curl -s --location --request GET "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=$1&date=$dt" --header 'Host: cdn-api.co-vin.in' --header 'User-Agent: Mozilla' --header 'Cookie: troute=t1;'  | /usr/local/bin/jq '.centers' | /usr/local/bin/jq '.[] | {pincode: .pincode, date: .sessions[].date, min_age_limit: .sessions[].min_age_limit, available_capacity: .sessions[].available_capacity} | select(.available_capacity > 0)' | xargs`
+  temp=`/usr/bin/curl -s --location --request GET "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=$1&date=$dt" --header 'Host: cdn-api.co-vin.in' --header 'User-Agent: Mozilla' --header 'Cookie: troute=t1;'  | /usr/local/bin/jq '.centers | [.[] | {pincode: .pincode, date: .sessions[].date, min_age_limit: .sessions[].min_age_limit, available_capacity: .sessions[].available_capacity} | select(.available_capacity > 0)] | unique_by(.pincode,.date)'`
 
-  results="$results$temp"
+  results="[ ${results},${temp} ]"
+  results=`echo $results | jq 'flatten'`
   inc=`expr $ctr \* 7`
   dt=`date -v +"$inc"d +"%d-%m-%Y"`
   ctr=`expr $ctr + 1`
 done
 
-result=`echo -n $results | wc -m`
-if [ $result -gt 0 ]
+if [ ${#results} -gt 2  ]
 then
   osascript -e 'display notification "45+ vaccine available" with title "Vaccine" subtitle "45+ vaccine available" sound name "Glass"'
   echo "$(date) Slots available"
